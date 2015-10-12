ef4b12df0a65ee17be33b7956660c2ec3dc648c2
Andrew Minion <andrew@andrewrminion.com>
2015-10-12 08:44:22 -0400

    don’t require resume upload

--- js/form-fixes.js
+++ js/form-fixes.js
@@ -1,7 +1,7 @@
 jQuery( document ).ready( function() {
-    jQuery( 'label[for="candidate_name"], label[for="application_attachment"]' ).append( ': <span class="ninja-forms-req-symbol">*</span>' );
-    jQuery( 'fieldset-candidate_email .required-field' ).removeClass( 'required-field' );
-    jQuery( 'input[name="candidate_email"]' ).attr( 'required', false );
+    jQuery( 'label[for="candidate_name"]' ).append( ': <span class="ninja-forms-req-symbol">*</span>' );
+    jQuery( 'fieldset-candidate_email .required-field, fieldset-application_attachment .required-field' ).removeClass( 'required-field' );
+    jQuery( 'input[name="candidate_email"], input[name="application_attachment[]"]' ).attr( 'required', false );
     jQuery( 'div.ninja-forms-required-items' ).remove();
     jQuery( '.ninja-forms-req, input#application_attachment' ).attr( 'required', true );
     if ( jQuery( '.ninja-forms-cont' ) ) {
